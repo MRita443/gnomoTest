e6dd9bc7c93535594d8fc1deb75d6cab7bbaef9f
Eduardo <edurocadas@gmail.com>
2022-11-24 15:04:02 +0000

    started exercise 3

---
+++ tokenring.c
@@ -0,0 +1,21 @@
+
+
+
+
+int main(int argc,char* argv[])
+ 
+int numberProcesses = argv[1];
+float blockProb = argv[2];
+int sleepTime = argv[3];
+int token = 0;
+
+int fd;
+
+for(int i= 0; i<numberProcesses; i++{
+	name = fstring("pipe%dto%d", i+1, i+2);				
+	int mkfifo( name, 0666);
+})
+name = fstring("pipe%dto%d", numberProcesses, 1);
+int make
+
+
