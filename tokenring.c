4d7e4c0e2fa946189b4a47eaa42ac9204190f91d
Rita Lopes <up202108819@up.pt>
2022-11-24 15:26:08 +0000

    Progressed tokenring

--- tokenring.c
+++ tokenring.c
@@ -1,21 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
 
+int main(int argc, char *argv[])
+{
 
+	int numberProcesses = atoi(argv[1]);
+	float blockProb = atof(argv[2]);
+	unsigned sleepTime = atoi(argv[3]);
+	int token = 0;
+	pid_t pid;
+	int fileDescriptor;
 
+	for (int i = 0; i < numberProcesses; i++)
+	{
+		char pipeName[] = fstring("pipe%dto%d", i + 1, i + 2);
+		if (mkfifo(pipeName, 0666) != 0)
+		{
+			printf("Unable to create a fifo; errno=%d\n", errno);
+			return EXIT_FAILURE;
+		}
+	}
 
-int main(int argc,char* argv[])
- 
-int numberProcesses = argv[1];
-float blockProb = argv[2];
-int sleepTime = argv[3];
-int token = 0;
+	char lastPipeName[] = fstring("pipe%dto%d", numberProcesses, 1);
+	if (mkfifo(lastPipeName, 0666) != 0)
+	{
+		printf("Unable to create a fifo; errno=%d\n", errno);
+		return EXIT_FAILURE;
+	}
 
-int fd;
-
-for(int i= 0; i<numberProcesses; i++{
-	name = fstring("pipe%dto%d", i+1, i+2);				
-	int mkfifo( name, 0666);
-})
-name = fstring("pipe%dto%d", numberProcesses, 1);
-int make
-
-
+	if ((pid = fork()) < 0)
+	{
+		perror("fork error");
+		exit(EXIT_FAILURE);
+	}
+	else if (pid > 0)
+	{
+	}
+	else if (pid == 0)
+	{
+		
+	}
+	return EXIT_SUCCESS;
+}
