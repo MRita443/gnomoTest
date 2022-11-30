30204ee931ae890c4efcfe7343567adca7a2a398
Rita Lopes <up202108819@up.pt>
2022-11-30 17:42:40 +0000

    Fixed 3

--- tokenring.c
+++ tokenring.c
@@ -6,22 +6,47 @@
 #include <sys/stat.h>
 #include <sys/wait.h>
 #include <fcntl.h>
+#include <time.h>
+
+char **pipes;
+
+int child(int processID, int blockProb, int sleepTime)
+{
+	
+	int token = -1;
+	srand(time(NULL) ^ getpid());
+	int fd = open(pipes[processID - 1], O_RDONLY);
+	int fw = open(pipes[processID], O_WRONLY);
+	while (1)
+	{
+		if(read(fd, &token, sizeof(int)) == -1){
+			printf("p%d error while reading\n", processID);
+		}
+		int probability = rand() % 100;
+		if (probability <= blockProb*100)
+		{
+			printf("[p%d] lock on token (val = %d)\n", processID, token);
+			sleep(sleepTime);
+			printf("[p%d] unlock token\n", processID);
+		}
+		token++;
+		write(fw, &token, sizeof(int));
+	}
+}
 
 int main(int argc, char *argv[])
 {
-	int numberProcesses = atoi(argv[1]);
-	int blockProb = atof(argv[2]) * 100;
+	srand(time(NULL) ^ getpid());
+	int numberProcesses = atoi(argv[1]) + 1; //lmao
+	pipes = (char **)malloc(sizeof(char **) * numberProcesses);
+	int blockProb = atof(argv[2]);
 	unsigned sleepTime = atoi(argv[3]);
-	char *pipes[numberProcesses];
 	int token = 0;
-	int currentPipe = 0;
 	pid_t pid;
-	int fd;
-	int fileDescriptor;
 
 	for (int i = 0; i < numberProcesses - 1; i++)
 	{
-		char pipeName[25];
+		char * pipeName = (char *) malloc(20);
 		sprintf(pipeName, "pipe%dto%d", i + 1, i + 2);
 
 		if (mkfifo(pipeName, 0666) == -1)
@@ -47,38 +72,33 @@ int main(int argc, char *argv[])
 	{
 		pipes[numberProcesses - 1] = lastPipeName; // Adiciona o último pipe criado à lista de pipes
 	}
-	while (1)
+	for (int i = 2; i <= numberProcesses; i++)
 	{
 		if ((pid = fork()) < 0)
 		{
 			perror("fork error");
 			exit(EXIT_FAILURE);
 		}
-		else if (pid > 0)
-		{
-			fd = open(pipes[currentPipe], O_WRONLY);
-			write(fd, token, sizeof(int));
-			close(fd);
-
-			int status;
-			wait(&status);
-		}
 		else if (pid == 0)
 		{
-			fd = open(pipes[currentPipe], O_RDONLY);
-			read(fd, token, sizeof(int));
-			float probability = (rand() * blockProb) % 1;
-			if (probability <= blockProb)
-			{
-				printf("[p%d] lock on token (val = %d)\n", (currentPipe + 2) % numberProcesses, token);
-				sleep(sleepTime);
-				printf("[p%d] unlock token\n", (currentPipe + 2) % numberProcesses);
-			}
-			close(fd);
-			token++;
-			currentPipe++;
-			currentPipe %= numberProcesses; // No caso, de o já ter sido utilizado o último pipe, voltar ao primeiro
+			child(i-1, blockProb, sleepTime);
 		}
+	}
+	int fw = open(pipes[0], O_WRONLY);
+	write(fw, &token, sizeof(int));
+	int fd = open(pipes[numberProcesses - 1], O_RDONLY);
+	while (1)
+	{
+		read(fd, &token, sizeof(int));
+		int probability = rand() % 100;
+		if (probability <= blockProb*100)
+		{
+			printf("[p%d] lock on token (val = %d)\n", 1, token);
+			sleep(sleepTime);
+			printf("[p%d] unlock token\n", 1);
+		}
+		token++;
+		write(fw, &token, sizeof(int));
 	}
 	return EXIT_SUCCESS;
 }
