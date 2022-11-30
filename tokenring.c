3b9cd1f841e0b7377abc5d7f34befe0d851613fd
Rita Lopes <up202108819@up.pt>
2022-11-30 13:11:25 +0000

    Finished ex3

--- tokenring.c
+++ tokenring.c
@@ -9,9 +9,8 @@
 
 int main(int argc, char *argv[])
 {
-	
 	int numberProcesses = atoi(argv[1]);
-	float blockProb = atof(argv[2]);
+	int blockProb = atof(argv[2]) * 100;
 	unsigned sleepTime = atoi(argv[3]);
 	char *pipes[numberProcesses];
 	int token = 0;
@@ -22,10 +21,10 @@ int main(int argc, char *argv[])
 
 	for (int i = 0; i < numberProcesses - 1; i++)
 	{
-		char* pipeName;
+		char pipeName[25];
 		sprintf(pipeName, "pipe%dto%d", i + 1, i + 2);
 
-		if (mkfifo(pipeName, 0666) != 0)
+		if (mkfifo(pipeName, 0666) == -1)
 		{
 			printf("Unable to create a fifo; errno=%d\n", errno);
 			return EXIT_FAILURE;
@@ -36,37 +35,50 @@ int main(int argc, char *argv[])
 		}
 	}
 
-	char* lastPipeName;
-	sprintf("pipe%dto%d", numberProcesses, 1);
+	char lastPipeName[25];
+	sprintf(lastPipeName, "pipe%dto%d", numberProcesses, 1);
 
-	if (mkfifo(lastPipeName, 0666) != 0)
+	if (mkfifo(lastPipeName, 0666) == -1)
 	{
 		printf("Unable to create a fifo; errno=%d\n", errno);
 		return EXIT_FAILURE;
 	}
 	else
 	{
-		pipes[numberProcesses - 1] = lastPipeName; //Adiciona o último pipe criado à lista de pipes
+		pipes[numberProcesses - 1] = lastPipeName; // Adiciona o último pipe criado à lista de pipes
 	}
+	while (1)
+	{
+		if ((pid = fork()) < 0)
+		{
+			perror("fork error");
+			exit(EXIT_FAILURE);
+		}
+		else if (pid > 0)
+		{
+			fd = open(pipes[currentPipe], O_WRONLY);
+			write(fd, token, sizeof(int));
+			close(fd);
 
-	if ((pid = fork()) < 0)
-	{
-		perror("fork error");
-		exit(EXIT_FAILURE);
-	}
-	else if (pid > 0)
-	{
-		fd = open(pipes[currentPipe], O_WRONLY);
-		write(fd, token, sizeof(int));
-		close(fd);
-	}
-	else if (pid == 0)
-	{
-		fd = open(pipes[currentPipe++], O_RDONLY);
-		currentPipe %= numberProcesses; //No caso, de o já ter sido utilizado o último pipe, voltar ao primeiro
-		read(fd, token, sizeof(int));
-		printf(token);
-		close(fd);
+			int status;
+			wait(&status);
+		}
+		else if (pid == 0)
+		{
+			fd = open(pipes[currentPipe], O_RDONLY);
+			read(fd, token, sizeof(int));
+			float probability = (rand() * blockProb) % 1;
+			if (probability <= blockProb)
+			{
+				printf("[p%d] lock on token (val = %d)\n", (currentPipe + 2) % numberProcesses, token);
+				sleep(sleepTime);
+				printf("[p%d] unlock token\n", (currentPipe + 2) % numberProcesses);
+			}
+			close(fd);
+			token++;
+			currentPipe++;
+			currentPipe %= numberProcesses; // No caso, de o já ter sido utilizado o último pipe, voltar ao primeiro
+		}
 	}
 	return EXIT_SUCCESS;
 }
