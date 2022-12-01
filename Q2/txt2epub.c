f8b16b93f8cbfef19da7925d12381761974769ad
Eduardo <edurocadas@gmail.com>
2022-12-01 17:25:54 +0000

    Commented Q2 and Q3

--- Q2/txt2epub.c
+++ Q2/txt2epub.c
@@ -12,27 +12,27 @@ int main(int argc, char *argv[])
     args[0] = "zip";
     args[1] = "ebooks.zip";
 
-    for (int i = 1; i < argc; i++)
+    for (int i = 1; i < argc; i++) // O ciclo repete-se para cada ficheiro .txt
     {
         char *argumentCopy = (char *)malloc(strlen(argv[i]) + 1);
-        strcpy(argumentCopy, argv[i]);
+        strcpy(argumentCopy, argv[i]); // Copia o ficheiro .txt para uma variável temporária
 
         char *fileNameTxt = argv[i];
-        char *fileNameEpub = strtok(argumentCopy, ".");
-        strcat(fileNameEpub, ".epub"); // Add .epub
+        char *fileNameEpub = strtok(argumentCopy, "."); // Quebra a variável temporária numa série de tokens usando o "." como delimitador
+        strcat(fileNameEpub, ".epub"); // Adiciona .epub
 
-        if ((pid = fork()) < 0)
+        if ((pid = fork()) < 0) // Dá erro se a criação do processo não for bem sucedida
         {
             perror("Fork error");
             exit(EXIT_FAILURE);
         }
-        else if (pid == 0)
+        else if (pid == 0) // Converte um ficheiro .txt em .epub usando o pandoc
         {
             char *pandocArgs[] = {"pandoc", fileNameTxt, "-o", fileNameEpub, NULL};
             printf("[%d] converting %s\n", getppid(), fileNameTxt);
             execvp("pandoc", pandocArgs);
         }
-        else if (pid > 0)
+        else if (pid > 0) // Adiciona o ficheiro .epub
         {
             int status;
             if (wait(&status) < 0)
--- Q3/tokenring.c
+++ Q3/tokenring.c
@@ -8,11 +8,17 @@
 #include <fcntl.h>
 #include <time.h>
 
+/*
+argv[1] -> Número de pipes
+argv[2] -> Probabilidade de bloquear
+argv[3] -> Tempo de bloquieo
+*/
+
 char **pipes;
 
 int main(int argc, char *argv[])
 {
-	if (argc != 4)
+	if (argc != 4) // Confirma que o número de argumentos passados é o correcto, ou seja, se foi chamada a função e os seus 3 parâmetros 
 	{
 		printf("Insert the correct number of arguments. tokenring <number of processes> <blocking probability> <block time>\n");
 		return EXIT_FAILURE;
@@ -24,12 +30,12 @@ int main(int argc, char *argv[])
 	int blockProb = atof(argv[2]);
 	unsigned sleepTime = atoi(argv[3]);
 
-	pipes = (char **)malloc(sizeof(char *) * numberProcesses);
+	pipes = (char **)malloc(sizeof(char *) * numberProcesses); // Dá o tamanho
 
 	int token = 0;
 	pid_t pid;
 
-	for (int i = 0; i < numberProcesses; i++)
+	for (int i = 0; i < numberProcesses; i++) // Cria os named pipes 
 	{
 		char *pipeName = (char *)malloc(20);
 		sprintf(pipeName, "pipe%dto%d", i + 1, 1 + (i + 1) % numberProcesses);
@@ -47,21 +53,21 @@ int main(int argc, char *argv[])
 
 	for (int i = 1; i < numberProcesses; i++)
 	{
-		if ((pid = fork()) < 0)
+		if ((pid = fork()) < 0) // Dá erro se a criação do processo não for bem sucedida
 		{
 			perror("Fork error");
 			return EXIT_FAILURE;
 		}
-		else if (pid == 0)
+		else if (pid == 0) // Processo child
 		{
-			int fd = open(pipes[i - 1], O_RDONLY);
+			int fd = open(pipes[i - 1], O_RDONLY); // Lê primeiro 
 			if (fd < 0)
 			{
 				perror("Error opening pipe");
 				return EXIT_FAILURE;
 			}
 
-			int fw = open(pipes[i], O_WRONLY);
+			int fw = open(pipes[i], O_WRONLY); // Depois escreve
 			if (fw < 0)
 			{
 				perror("Error opening pipe");
@@ -76,13 +82,13 @@ int main(int argc, char *argv[])
 					return EXIT_FAILURE;
 				}
 				int probability = rand() % 100;
-				if (probability <= blockProb * 100)
+				if (probability <= blockProb * 100) // Bloqueia o envio do token. A blockProb passa a ser um inteiro de 0 a 100
 				{
 					printf("[p%d] Pid:%d lock on token (val = %d)\n", i + 1, getpid(), token);
 					sleep(sleepTime);
 					printf("[p%d] Pid:%d unlock token\n", i + 1, getpid());
 				}
-				token++;
+				token++; 
 				if ((write(fw, &token, sizeof(int))) != sizeof(token))
 				{
 					perror("Write error");
@@ -92,13 +98,13 @@ int main(int argc, char *argv[])
 		}
 		printf("Created child %d\n", pid);
 	}
-	int fw = open(pipes[0], O_WRONLY);
+	int fw = open(pipes[0], O_WRONLY); // No processo parent, considerado o primeiro processo, escreve primeiro, colocando o token em circulação
 	if (fw < 0)
 	{
 		perror("Error opening pipe");
 		return EXIT_FAILURE;
 	}
-	int fd = open(pipes[numberProcesses - 1], O_RDONLY);
+	int fd = open(pipes[numberProcesses - 1], O_RDONLY); // Depois lê
 	if (fd < 0)
 	{
 		perror("Error opening pipe");
@@ -107,7 +113,7 @@ int main(int argc, char *argv[])
 	while (1)
 	{
 		int probability = rand() % 100;
-		if (probability <= blockProb * 100)
+		if (probability <= blockProb * 100) // Bloqueia o envio do token. A blockProb passa a ser um inteiro de 0 a 100 
 		{
 			printf("[p%d] Pid:%d lock on token (val = %d)\n", 1, getpid(), token);
 			sleep(sleepTime);
