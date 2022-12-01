b377bfe53701a5f900b317a78043e0127bb26ea1
Eduardo <edurocadas@gmail.com>
2022-12-01 15:53:23 +0000

    Commented Q1

--- Q1/samples.c
+++ Q1/samples.c
@@ -4,9 +4,9 @@
 #include <string.h>
 
 /*
-argv[1] -> texto
-argv[2] -> número de fragmentos
-argv[3] -> tamanho dos fragmentos
+argv[1] -> Texto
+argv[2] -> Número de fragmentos
+argv[3] -> Tamanho dos fragmentos
 */
 
 long getSize(FILE *f)
@@ -19,12 +19,12 @@ long getSize(FILE *f)
 
 int main(int argc, char *argv[])
 {
-    if (argc != 4)
+    if (argc != 4) // Confirma que o número de argumentos passados é o correcto, ou seja, se foi chamada a função e os seus 3 parâmetros 
     {
         printf("Insert the correct number of arguments. samples <file name> <number of fragments> <size per fragment>\n");
         return EXIT_FAILURE;
     }
-    FILE *f = fopen(argv[1], "r");
+    FILE *f = fopen(argv[1], "r"); // Abre o ficheiro para ser lido
     if (!f)
     {
         perror("Error opening file");
@@ -43,15 +43,15 @@ int main(int argc, char *argv[])
         return EXIT_FAILURE;
     }
 
-    srandom(0);
+    srandom(0); // Cria uma seed para ir buscar pseudo-aleatoriamente as posições dos fragmentos
     long size = getSize(f);
     for (int i = 0; i < m; i++)
     {
-        int pos = (random() % (size - n));
+        int pos = (random() % (size - n)); // Gera uma posição inicial com garantia que está a pelo menos n caracteres do fim do ficheiro cada vez que se quer criar um fragmento
         fseek(f, pos, SEEK_SET);
-        char *buffer = malloc(n);
+        char *buffer = malloc(n); // Cria um pointer chamado buffer para uma memória alocada de tamanho n cada vez que se quer criar um fragmento
         fread(buffer, 1, n, f);
-        printf(">%s<\n", buffer);
+        printf(">%s<\n", buffer); // Imprime o fragmento
     }
     if (fclose(f) != 0)
     {
